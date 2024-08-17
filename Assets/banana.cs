f03768f11abb4f5994afd2fb57dd59e48dd50dde
Danylo <dbashinskiy@miem.hse.ru>
2024-08-17 17:51:44 +0300

    Init commit

---
+++ Assets/banana.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class banana : MonoBehaviour
+{
+    public float speed;
+    
+    void Update()
+    {
+        transform.position += new Vector3(speed / 100, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Wolf"))
+        {
+            other.gameObject.GetComponent<hunger>().AddHunger(0.1f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/hunger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hunger : MonoBehaviour
+{
+    public Image image;
+
+    public void AddHunger(float amount)
+    {
+        image.fillAmount = Mathf.Clamp01(image.fillAmount + amount);
+    }
+    
+}
